13009f57322dc68e9a0e86ec4b0a085179fa927b
Vibarbara <vivifavato@gmail.com>
2023-01-11 00:22:34 -0300

    Alterações nome arquivo e js

--- atividade_10.01.23/script.js
+++ atividade_10.01.23/script.js
@@ -3,6 +3,24 @@ const navPerfil = document.getElementById("nav-perfil");
 const linkDados = document.getElementById("link-perfil-dados");
 const voltar = document.getElementById("voltar");
 
+window.addEventListener('keydown', (event)=> {
+    console.log(linkDados);
+    
+  if (event.code === 'Digit1') {
+      if (navPerfil.style.display == 'block') {
+      linkDados.click();
+   } else {
+      navPerfil.style.display = 'block';
+   }
+  } 
+  if (event.code === 'Escape') {
+      navPerfil.style.display = 'none';
+  }
+  if (event.code === 'Backspace') {
+      voltar.click();
+  }
+})
+
 //linkPerfil.addEventListener("mouseover", ()=> {
 //  navPerfil.style.display = "block"
 //})
@@ -25,23 +43,7 @@ const voltar = document.getElementById("voltar");
 // } 
 //})
 
-window.addEventListener('keydown', (event)=> {
-  console.log(linkDados);
-  
-if (event.code === 'Digit1') {
-    if (navPerfil.style.display == 'block') {
-    linkDados.click();
- } else {
-    navPerfil.style.display = 'block';
- }
-} 
-if (event.code === 'Escape') {
-    navPerfil.style.display = 'none';
-}
-if (event.code === 'Backspace') {
-    voltar.click();
-}
-})
+
 
 
 
