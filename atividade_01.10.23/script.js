620cc0dddbaf825fb5ceba5c6b4f6311ee2e527c
Vibarbara <vivifavato@gmail.com>
2023-01-10 23:51:59 -0300

    Organização pastas atividades

---
+++ atividade_01.10.23/script.js
@@ -0,0 +1,49 @@
+const linkPerfil = document.getElementById("link-perfil");
+const navPerfil = document.getElementById("nav-perfil");
+const linkDados = document.getElementById("link-perfil-dados");
+const voltar = document.getElementById("voltar");
+
+//linkPerfil.addEventListener("mouseover", ()=> {
+//  navPerfil.style.display = "block"
+//})
+
+//window.addEventListener('keyup', (event)=> {
+//  console.log(event);
+
+//  if (event.key === '1') {
+//    navPerfil.style.display = "block";
+//  } if (event.key === 'Escape') {
+//    navPerfil.style.display = "none";
+//  } 
+//})
+
+//window.addEventListener('keyup', (event)=> {
+//  console.log(event);
+
+//if (event.code === 'Digit1') {
+//    navPerfil.style.display = "block";
+// } 
+//})
+
+window.addEventListener('keydown', (event)=> {
+  console.log(linkDados);
+  
+if (event.code === 'Digit1') {
+    if (navPerfil.style.display == 'block') {
+    linkDados.click();
+ } else {
+    navPerfil.style.display = 'block';
+ }
+} 
+if (event.code === 'Escape') {
+    navPerfil.style.display = 'none';
+}
+if (event.code === 'Backspace') {
+    voltar.click();
+}
+})
+
+
+
+
+
---
+++ atividade_23.12.22/script.js
@@ -0,0 +1,50 @@
+const quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+const valorSubtotal = document.getElementById("valor-subtotal");
+const subItens = document.getElementById("btn-subtrair-produto-01")
+const addItens = document.getElementById("btn-adicionar-produto-01");
+const quantidadeItem = document.getElementById("quantidade-produto-01");
+/*const title = document.getElementsByTagName("h2");*/
+
+console.log(quantidadeItem.value);
+
+const valorItemInfo = {
+  valor: 45.90,
+};
+
+//const subtotalInfo = {
+//  valor: 11.66,
+//};
+
+subItens.addEventListener("click", subtrairItem);
+addItens.addEventListener("click", adicionarItem);
+
+function calculoValorTotal(quantidadeItem) {
+  if (quantidadeItem > 0) {
+    valorSubtotal.innerText = (valorItemInfo.valor * quantidadeItem).toFixed(2);
+  } else {
+    valorSubtotal.innerText = 0
+  }
+}
+
+function updateSubTotalText() {
+  quantidadeSubtotal.innerText = quantidadeItem.value == 1? `${quantidadeItem.value} item` : `${quantidadeItem.value} itens`;
+  calculoValorTotal(quantidadeItem.value);
+}
+
+function subtrairItem() {
+  if (quantidadeItem.value > 0) {
+    quantidadeItem.value = Number(quantidadeItem.value) - 1;
+  }
+  updateSubTotalText();
+}
+
+function adicionarItem() {
+  if (quantidadeItem.value < 10) {
+    quantidadeItem.value = Number(quantidadeItem.value) + 1;
+  } //else {
+  //  title[1].innerText = "MÁXIMO DE 10 ITENS";
+  //}*/
+  updateSubTotalText();
+}
+
+
